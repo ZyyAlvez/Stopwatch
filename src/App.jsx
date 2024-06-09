81518675a16fe5c794684bce558ea4c1b8ff1297
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-06-09 23:31:09 +0800

    Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Button from './component/Button.jsx'
 
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <h1>{"00:00:00"}</h1>
+      <div className="button-holder">
+        <Button title="Start"/>
+        <Button title="Stop"/>
+        <Button title="Restart"/>
+      </div>
+    </div>
   )
 }
 
